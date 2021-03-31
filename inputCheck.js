caa1b8f335453bad7ed45c30c2251a5774c881d9
HidehikoKondo <dev@udonko.net>
2021-03-31 23:12:10 +0900

    入力チェックのリファクタ

--- inputCheck.js
+++ inputCheck.js
@@ -1,6 +1,8 @@
 function inputCheck() {
+    //チェック結果 true:入力チェックOK　false:未記入アリ
     var result = true;
 
+    //選択した収支のラジオボタンの取得
     var radio = document.getElementsByName("balance");
     var balance;
     for (let i = 0; i < radio.length; i++) {
@@ -9,12 +11,13 @@ function inputCheck() {
             break;
         }
     }
+    //日付、カテゴリ、金額、メモの取得
     var date = document.getElementById("date").value;
     var category = document.getElementById("category").value;
     var amount = document.getElementById("amount").value;
     var memo = document.getElementById("memo").value;
 
-    //日付が未記入
+    //入力チェック。未記入があればresultをfalseにする
     if (date == "") {
         result = false;
         alert("日付が未記入です");
@@ -31,6 +34,7 @@ function inputCheck() {
         return;
     }
     if (memo == "") {
+        result = false;
         alert("メモが未記入です");
         return;
     }
